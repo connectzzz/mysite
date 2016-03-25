dfa59642d0ca8689a8509bdffdd0202351ab70ab
Maxim Zzzz <z_connect@mail.ru>
2016-03-25 14:37:10 +0300

    Создан конфигурационный файл

--- www/eshop/inc/config.inc.php
+++ www/eshop/inc/config.inc.php
@@ -1,1 +1,8 @@
 <?php
+define('DB_HOST','localhost');
+define('DB_LOGIN','root');
+define('DB_PASSWORD','');
+define('DB_NAME','eshop');
+define('ORDERS_LOG','orders.log');
+$basket=[];  // массив для хранения корзины пользователя
+$count=0;    // переменная для хранения количества товаров в корзине пользователя

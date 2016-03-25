314f08d15c67d03e6d05329e44eb1e7eae97d9bc
Maxim Zzzz <z_connect@mail.ru>
2016-03-25 14:51:08 +0300

    В конфигурационном файле реализовано подключение к БД

--- www/eshop/inc/config.inc.php
+++ www/eshop/inc/config.inc.php
@@ -6,3 +6,10 @@ define('DB_NAME','eshop');
 define('ORDERS_LOG','orders.log');
 $basket=[];  // массив для хранения корзины пользователя
 $count=0;    // переменная для хранения количества товаров в корзине пользователя
+
+$link=mysqli_connect(DB_HOST,DB_LOGIN,DB_PASSWORD,DB_NAME);
+if (!$link){
+    echo 'Ошибка подключения: '
+        .mysqli_connect_errno(). ' '
+        .mysqli_connect_error();
+}

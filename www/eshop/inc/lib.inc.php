2ae39200a2f82728ceac9bcec9dcf8279f8222cb
Maxim Zzzz <z_connect@mail.ru>
2016-03-25 15:18:15 +0300

    В библиотеке реализована функция добавления товара в каталог addItemToCatalog

--- www/eshop/inc/lib.inc.php
+++ www/eshop/inc/lib.inc.php
@@ -1,1 +1,14 @@
 <?php
+
+function addItemToCatalog($title,$author,$pubyear,$price)
+{
+    $sql='INSERT INTO eshop (title,author,pubyear,price) VALUES (?,?,?,?)';
+
+    if (!$stmt=mysqli_prepare($link,$sql)){
+        return false;
+    }
+    mysqli_stmt_bind_param($stmt,'ssii',$title,$author,$pubyear,$price);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    return true;
+}

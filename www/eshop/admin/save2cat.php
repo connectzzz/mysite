da930a4aff282adb0d5d1bab55632ade2f468759
Maxim Zzzz <z_connect@mail.ru>
2016-03-26 12:07:56 +0300

    Реализована обработка полученных данных из веб формы

--- www/eshop/admin/save2cat.php
+++ www/eshop/admin/save2cat.php
@@ -3,3 +3,17 @@
 	require "secure/session.inc.php";
 	require "../inc/lib.inc.php";
 	require "../inc/config.inc.php";
+
+
+if ($title=$_POST['title']&&$author=$_POST['author']&&
+        $pubyear=$_POST['pubyear']&&$price=$_POST['price'])
+{
+
+    if(addItemToCatalog($title,$author,$pubyear,$price))
+    {
+        echo 'Произошла ошибка при добавлении товара в коталог';
+    }else{
+        header("Locarion: add2cat.php");
+        exit;
+    }
+}
